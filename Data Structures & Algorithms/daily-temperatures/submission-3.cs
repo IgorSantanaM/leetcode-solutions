a9e7054b80a7f4c85938156d8b501f77eeb48e7e
Igor Medeiros <118362935+IgorSantanaM@users.noreply.github.com>
2026-08-12 09:01:03 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+       int[] res = new int[temperatures.Length];
+       Stack<(int temperature, int index)> stack = new();
+       int lastCount = temperatures[^1];
+
+       for(int i = 0; i < temperatures.Length; i++)
+       {
+            if(temperatures[i] == lastCount)
+                res[i] = 0;
+
+            while(stack.Count > 0 && stack.Peek().temperature < temperatures[i])
+            {
+                var pair = stack.Pop();
+                res[pair.index] = i - pair.index; 
+            }
+            stack.Push((temperatures[i], i));
+       }
+
+       return res;
+    }
+}
