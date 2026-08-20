86d81accca4f3794e0bfa8d399fcca1e97a5b654
Igor Medeiros <118362935+IgorSantanaM@users.noreply.github.com>
2026-08-20 10:09:40 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) {
+       var pairs = new int[position.Length][];
+
+       for(int i = 0; i < position.Length; i++)
+       {
+            pairs[i] = new int[]{position[i], speed[i]};
+       }
+
+        Array.Sort(pairs, (a, b) =>
+            b[0].CompareTo(a[0]));
+
+       Stack<double> stack = new();
+    
+        foreach(var pair in pairs)
+        {
+            stack.Push((double)(target - pair[0]) / pair[1]);
+
+            if(stack.Count >= 2 && stack.Peek() <= stack.ElementAt(1))
+                stack.Pop();
+        }
+
+        return stack.Count;
+
+    }
+}
